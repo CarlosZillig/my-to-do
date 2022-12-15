c76c83e5164821cbb799c15e9d7b6ad3a06c30c3
Carlos Zillig <carloszilligg@gmail.com>
2022-12-15 10:34:44 -0300

    Adicionando o icone de edição da task, definindo o dark-mode para o site e alterando o design

--- src/Pages/Tasks/List/index.jsx
+++ src/Pages/Tasks/List/index.jsx
@@ -1,22 +1,29 @@
 import styles from './List.module.scss'
 import { AiOutlineDelete } from 'react-icons/ai'
+import { BiEdit } from 'react-icons/bi'
 
-const List = ({listTask}) => {
-    return(
+const List = ({ listTask }) => {
+    return (
         <div className={styles.container}>
             <h2 className={styles.title}>Suas tarefas</h2>
             <section className={styles['list-section']}>
                 <ul className={styles.list}>
-                    {listTask !== null && 
+                    {listTask !== null &&
                         listTask.map(item => (
                             <li className={styles['list-item']}>
-                                <h4 className={styles['item-title']}>
-                                    {item.task}
-                                </h4>
+                                <div className={styles['item-container']}>
+                                    <h4 className={styles['item-title']}>
+                                        {item.task}
+                                        {console.log(item.task.length)}
+                                    </h4>
+                                    <div className={styles['item-config']}>
+                                        <BiEdit className={styles['item-edit']}/>
+                                        <AiOutlineDelete className={styles['item-delete']} />
+                                    </div>
+                                </div>
                                 <p className={styles['item-desc']}>
                                     {item.desc}
                                 </p>
-                                <AiOutlineDelete className={styles['item-delete']}/>
                             </li>
                         ))
                     }
