b13ee72a35a1753e0f1846acd1ffb5c6f89322be
Carlos Zillig <carloszilligg@gmail.com>
2023-02-13 12:02:25 -0300

    style: Adicionando o estilo básico da lixeira

--- src/Pages/Dump/ListDump/index.jsx
+++ src/Pages/Dump/ListDump/index.jsx
@@ -1,15 +1,24 @@
 import useTrashList from '../../../states/hooks/trashList/useTrashList';
-import style from './ListDump.module.scss'
+import styles from './ListDump.module.scss'
 
 const ListDump = () => {
   const trash = useTrashList();
 
   return (
-    <div className={style['list-dump']}>
-      <ul>
-        {trash.length == 0 && <h1>Sua lixeira está limpa</h1>}
-        {trash.map((item) => (
-          <h1>{item.task}</h1>
+    <div className={styles['list-dump--container']}>
+      <div className={styles['list-total']}>Total: {trash.length}</div>
+      <ul className={styles['list-dump']}>
+        {trash.length == 0 && <h1 className={styles['dump-clear']}>Sua lixeira está limpa</h1>}
+        {trash.map((item, index) => (
+          <li className={styles['list-item']} key={item.id}>
+            <div className={styles['item-container']}>
+              <span className={styles['item-position']}>{index + 1}.</span>
+              <span className={styles['item-title']}>{item.task}</span>
+            </div>
+            <p className={styles['item-desc']} style={{ display: 'none' }}>
+              {item.desc}
+            </p>
+          </li>
         ))}
       </ul>
     </div>
